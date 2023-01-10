8d0cfcd5704125de9c2c08a35a8f8cbbc9dea3d8
DenisPepper <thech@yandex.ru>
2023-01-10 14:59:15 +0300

    Modify the existing declarations

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -52,3 +52,15 @@ c = c + " String!";
 
 
 
+/* Modify the existing declarations and assignments so their names use camelCase. */
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+
