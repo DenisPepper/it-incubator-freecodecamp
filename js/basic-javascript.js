84f5f5e62d8fc5f7b17c28b56c458f4188c3851f
DenisPepper <thech@yandex.ru>
2023-01-10 16:52:16 +0300

    use the -= operator

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -168,3 +168,18 @@ let c = 12;
 a += 12;
 b += 9;
 c += 7;
+
+
+
+
+
+/*Convert the assignments for a, b, and c to use the -= operator. */
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
