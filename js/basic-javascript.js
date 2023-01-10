b76df4c1e757f5311e1078efd701963c5adb1429
DenisPepper <thech@yandex.ru>
2023-01-10 14:18:59 +0300

    Assign the contents of a to variable b

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -13,3 +13,13 @@ var a;
 // Only change code below this line
 
 a = 7;
+
+
+/* 004 Assign the contents of a to variable b. */
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+b = a;
