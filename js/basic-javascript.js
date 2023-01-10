21fda8efe81264b3d84d812e95a2208ee69a4297
DenisPepper <thech@yandex.ru>
2023-01-10 16:06:22 +0300

    Change the code

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -69,3 +69,16 @@ titleCaseOver = 9000;
 /* Update the code so it only uses the let keyword. */
 let catName = "Oliver";
 let catSound = "Meow!";
+
+
+
+
+/* Change the code so that all variables are declared using let or const. 
+Use let when you want the variable to change, and const when you want the variable to remain constant. 
+Also, rename variables declared with const to conform to common practices. 
+Do not change the strings assigned to the variables. */
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
