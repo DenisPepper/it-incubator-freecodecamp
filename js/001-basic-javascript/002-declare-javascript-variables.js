621391b9d0c25c509f41e2a0c12353d41dfaa93e
DenisPepper <thech@yandex.ru>
2023-01-10 14:05:30 +0300

    creates a variable

---
+++ js/001-basic-javascript/002-declare-javascript-variables.js
@@ -0,0 +1,5 @@
+/* creates a variable called ourName. In JavaScript we end statements with semicolons. 
+Variable names can be made up of numbers, letters, and $ or _, but may not contain spaces or start with a number.
+ */
+
+var myName;
