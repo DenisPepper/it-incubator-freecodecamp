6e60ff0e252223d398f418011d80fe4e13c63e40
DenisPepper <thech@yandex.ru>
2023-01-11 15:36:34 +0300

    eclare a local variable myVar

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -549,3 +549,23 @@ function fun2() {
   }
   console.log(output);
 }
+
+
+
+
+
+
+/*
+The editor has two console.logs to help you see what is happening. Check the console as you code to see how it changes. 
+Declare a local variable myVar inside myLocalScope and run the tests.
+*/
+function myLocalScope() {
+    // Only change code below this line
+    let myVar5;
+    console.log('inside myLocalScope', myVar5);
+  }
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar5);
