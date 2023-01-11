5d72f8676a694c4c49d34482c8d299962e9cab39
DenisPepper <thech@yandex.ru>
2023-01-11 14:15:26 +0300

    Use bracket notation

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -289,3 +289,17 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 lastNameLength = lastName.length;
+
+
+
+
+
+/*
+Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+*/
+// Setup
+let firstLetterOfLastName = "";
+const lastName1 = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName1[0]; // Change this line
