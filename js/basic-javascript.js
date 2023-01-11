b2d6c043c7944c78a85c255be523b7764bdf3af1
DenisPepper <thech@yandex.ru>
2023-01-11 15:32:55 +0300

    Using let or const, declare a global variable

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -517,3 +517,35 @@ functionWithArgs(1, 2);
 Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
 */
 const timesFive = num => num * 5;
+
+
+
+
+
+
+
+/*
+Using let or const, declare a global variable named myGlobal outside of any function. Initialize it with a value of 10.
+Inside function fun1, assign 5 to oopsGlobal without using the var, let or const keywords.
+*/
+
+// Declare the myGlobal variable below this line
+const myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
