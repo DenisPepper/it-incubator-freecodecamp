fb525ac07919896bffe0e00726707babfaeb318b
DenisPepper <thech@yandex.ru>
2023-01-11 15:46:53 +0300

    last not resolved chellenge

--- js/0001-test.js
+++ js/0001-test.js
@@ -1,7 +1,7 @@
 export const test = () => {
     
     const p = document.createElement('p');
-    p.textContent = "Hello, world!"
+    p.textContent = "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/stand-in-line"
 
     const htmlBody = document.querySelector('body').appendChild(p);
 
