4c0b073d7ce1517cb9c42fa5552890fec2cc23f2
DenisPepper <thech@yandex.ru>
2023-01-11 15:09:17 +0300

    Using bracket notation

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -410,3 +410,18 @@ const myArray3 = [18, 64, 99];
 
 // Only change code below this line
 myArray3[0] = 45;
+
+
+
+
+/*
+Using bracket notation select an element from myArray such that myData is equal to 8.
+*/
+const myArray4 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData1 = myArray4[2][1];
