11f133d28a9d1e001085351ce8523c159d76d678
DenisPepper <thech@yandex.ru>
2023-01-11 15:27:31 +0300

    Create a function

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -499,3 +499,13 @@ myList.push(['potato', 0]);
 */
 const reusableFunction  = () => console.log('Hi World');
 reusableFunction();
+
+
+
+
+/*
+Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+Call the function with two numbers as arguments.
+*/
+const functionWithArgs = (a = 0, b = 0) => console.log(a + b);
+functionWithArgs(1, 2);
