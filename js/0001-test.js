6ca78dfe777384d966e990e6517572f53bb070e5
DenisPepper <thech@yandex.ru>
2023-01-11 15:53:18 +0300

    minor fixes

--- js/0001-test.js
+++ js/0001-test.js
@@ -1,8 +1,11 @@
 export const test = () => {
     
-    const p = document.createElement('p');
-    p.textContent = "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/stand-in-line"
+    const a = document.createElement('a');
+    a.href = 'https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/stand-in-line';
+    a.textContent = 'my last not resolved chellenge';
+    a.style.display = 'block';
+    a.style.paddingTop = '30px';
 
-    const htmlBody = document.querySelector('body').appendChild(p);
+    const htmlBody = document.querySelector('body').appendChild(a);
 
 };
