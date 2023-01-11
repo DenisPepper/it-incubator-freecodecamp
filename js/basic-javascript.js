b3de6ddbcf442b7bfb3fc000cc420edbd8955428
DenisPepper <thech@yandex.ru>
2023-01-11 14:47:23 +0300

    Correct the assignment to myStr

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -303,3 +303,19 @@ const lastName1 = "Lovelace";
 
 // Only change code below this line
 firstLetterOfLastName = lastName1[0]; // Change this line
+
+
+
+
+
+/*
+Correct the assignment to myStr so it contains the string value of Hello World 
+using the approach shown in the example above.
+*/
+
+// Setup
+let myStr22 = "Jello World";
+
+// Only change code below this line
+myStr22 = "Hello World"; // Change this line
+// Only change code above this line
