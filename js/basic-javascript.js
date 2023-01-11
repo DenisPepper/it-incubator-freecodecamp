d983564fb9d027f21303394d4aa89b3d78031979
DenisPepper <thech@yandex.ru>
2023-01-11 13:40:14 +0300

    Change the provided string

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -220,3 +220,11 @@ c /= 11;
 I am a "double quoted" string inside "double quotes".
 */
 const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+
+
+
+/* Change the provided string to a string with single quotes at the beginning and end and no escape characters. 
+const myStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+*/
+const myStr1 = '<a href="http://www.example.com" target="_blank">Link</a>';
