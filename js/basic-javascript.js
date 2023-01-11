05b251a5fef8d590925a5d2f2a54a2068d103685
DenisPepper <thech@yandex.ru>
2023-01-11 14:13:21 +0300

    Use the .length property

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -277,3 +277,15 @@ Set someAdjective to a string of at least 3 characters and append it to myStr us
 const someAdjective = "Hello";
 let myStr11 = "Learning to code is ";
 myStr11 += someAdjective;
+
+
+
+/*
+Use the .length property to set lastNameLength to the number of characters in lastName.
+*/
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
