9d35589f61959951580e2a8dfc24fe4c57485f3b
DenisPepper <thech@yandex.ru>
2023-01-12 16:30:30 +0300

    Logical Order in If Else Statements

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -858,3 +858,18 @@ function testElseIf(val) {
     return "Between 5 and 10";
   }
 };
+
+
+
+/*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/logical-order-in-if-else-statements
+*/
+function orderMyLogic(val) {
+  if (val < 10 && val >=5 ) {
+    return "Less than 10";
+  } else if (val < 5) {
+    return "Less than 5";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
