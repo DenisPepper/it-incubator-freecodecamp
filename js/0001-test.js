85159bf641fcb4a892909fbf8477706aff665b7e
DenisPepper <thech@yandex.ru>
2023-01-12 19:00:47 +0300

    marked next challenge

--- js/0001-test.js
+++ js/0001-test.js
@@ -1,7 +1,7 @@
 export const test = () => {
     
     const a = document.createElement('a');
-    a.href = 'https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/stand-in-line';
+    a.href = 'https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/testing-objects-for-properties';
     a.textContent = 'my last not resolved chellenge';
     a.style.display = 'block';
     a.style.paddingTop = '30px';
