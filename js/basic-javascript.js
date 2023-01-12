f147b323b9f1ceda62ed8bfd02c92e4474ad90df
DenisPepper <thech@yandex.ru>
2023-01-12 16:11:03 +0300

    Comparison with the Strict Inequality Operator

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -708,3 +708,15 @@ function testNotEqual(val) {
   }
   return "Equal";
 }
+
+
+
+/*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparison-with-the-strict-inequality-operator
+*/
+function testStrictNotEqual(val) {
+  if (val !==17) { 
+    return "Not Equal";
+  }
+  return "Equal";
+}
