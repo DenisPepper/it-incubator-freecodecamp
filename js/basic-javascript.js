2d1afdf725fe8989908442165826d5dd2535acb3
DenisPepper <thech@yandex.ru>
2023-01-12 16:21:53 +0300

    Comparisons with the Logical Or Operator

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -805,3 +805,19 @@ function testLogicalAnd(val) {
   // Only change code above this line
   return "No";
 }
+
+
+
+/*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparisons-with-the-logical-or-operator
+*/
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val <10 || val > 20) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
