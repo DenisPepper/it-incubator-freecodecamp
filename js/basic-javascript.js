a30f67bd6e1269a4e735072b6b8c7136cbd3b129
DenisPepper <thech@yandex.ru>
2023-01-12 18:44:24 +0300

    Accessing Object Properties with Bracket Notation

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -1161,3 +1161,19 @@ const testObj = {
 // Only change code below this line
 const hatValue = testObj.hat;      // Change this line
 const shirtValue = testObj.shirt;    // Change this line
+
+
+
+/*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/accessing-object-properties-with-bracket-notation
+*/
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj['an entree'];   // Change this line
+const drinkValue = testObj['the drink'];    // Change this line
