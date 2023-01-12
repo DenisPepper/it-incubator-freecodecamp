22de1d1f73f45219d2f722367d0e6a62e9e1a93b
DenisPepper <thech@yandex.ru>
2023-01-12 17:32:56 +0300

    Selecting from Many Options with Switch Statements

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -947,3 +947,29 @@ if (strokes === 1) {
   return result;
   // Only change code above this line
 }
+
+
+
+/*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/selecting-from-many-options-with-switch-statements
+*/
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case 1:
+    answer = "alpha";
+    break;
+   case 2:
+    answer = "beta";
+    break;
+     case 3:
+    answer = "gamma";
+    break;
+     case 4:
+    answer = "delta";
+    break;
+}
+  // Only change code above this line
+  return answer;
+}
