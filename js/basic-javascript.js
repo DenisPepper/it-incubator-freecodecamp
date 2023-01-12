47469fbd58eb9ea4ceb5455b8af96974961356bf
DenisPepper <thech@yandex.ru>
2023-01-12 16:15:37 +0300

    Comparison with the Less Than Operator

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -753,3 +753,20 @@ function testGreaterOrEqual(val) {
 
   return "Less than 10";
 }
+
+
+
+/*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparison-with-the-less-than-operator
+*/
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
