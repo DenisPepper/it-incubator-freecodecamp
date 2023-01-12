110c42ac9e11c346c90b6facea28b871a6604c7b
DenisPepper <thech@yandex.ru>
2023-01-12 18:52:21 +0300

    Delete Properties from a JavaScript Object

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -1223,3 +1223,20 @@ const myDog = {
 };
 
 myDog.bark = "woof";
+
+
+
+/*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/delete-properties-from-a-javascript-object
+*/
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails;
