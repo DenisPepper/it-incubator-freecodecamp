824fde633c4cf41bfbbeb5008ab79e6efd3f38ae
DenisPepper <thech@yandex.ru>
2023-01-12 17:49:19 +0300

    Returning Boolean Values from Functions

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -1063,3 +1063,16 @@ switch (val) {
 }
 
 chainToSwitch(7);
+
+
+
+/*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/returning-boolean-values-from-functions
+*/
+function isLess(a, b) {
+  // Only change code below this line
+  return a < b;
+  // Only change code above this line
+}
+
+isLess(10, 15);
