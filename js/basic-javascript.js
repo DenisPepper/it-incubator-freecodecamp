591b5d25ec8328ddd04c4cb5a17881185a8d578c
DenisPepper <thech@yandex.ru>
2023-01-12 16:14:05 +0300

    Comparison with the Greater Than Or Equal To

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -736,3 +736,20 @@ function testGreaterThan(val) {
 
   return "10 or Under";
 }
+
+
+
+/*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparison-with-the-greater-than-or-equal-to-operator
+*/
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
