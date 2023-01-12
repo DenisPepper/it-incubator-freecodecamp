3ef74e38a94a33f5262b35736020a5115ac2fd97
DenisPepper <thech@yandex.ru>
2023-01-12 16:23:43 +0300

    Introducing Else Statements

--- js/basic-javascript.js
+++ js/basic-javascript.js
@@ -821,3 +821,22 @@ function testLogicalOr(val) {
   // Only change code above this line
   return "Inside";
 }
+
+
+
+/*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/introducing-else-statements
+*/
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
